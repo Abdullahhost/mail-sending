8d763e25e77d442db9c5e7f7f22a36c78b6c00d4
Abdullah Al Mamun <abdullahgodboy@gmail.com>
2024-08-17 15:03:29 +0600

    access for front-end

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://mamun-hossain.vercel.app"],
     credentials: true,
   })
 );
