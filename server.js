d42fcd7c51ae7cf53f87da2347eed644883993ca
Abdullah Al Mamun <abdullahgodboy@gmail.com>
2024-08-17 14:02:56 +0600

    set up cors

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 import express from "express";
 import {} from "dotenv/config";
 
+import cors from "cors";
+
 import sendEmail from "./routes/index.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 8001;
 app.use("/sendMail", sendEmail);
-
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
